e12a9a28434ba9f3006d2677732b00c0b22bbaac
Călin Bogdan <calin.bogdan15@gmail.com>
2020-03-26 17:46:19 +0200

    Added areas and strokes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   axisBottom,
   axisLeft,
   axisRight,
-  axisTop,
+  line,
   scaleTime,
   scaleLinear,
   area,
@@ -26,15 +26,30 @@ const healthRecordsCreationDates = healthRecords
 
 const minMaxDates = extent(healthRecordsCreationDates);
 
-const recordsPerDay = Array.from(group(healthRecords, healthRecord => healthRecord.createdon));
-const numberOfRecordsPerDay = recordsPerDay
-  .map(([_, value]) => value.length);
+const recordsPerDay = Array.from(group(healthRecords, healthRecord => healthRecord.createdon))
+  .sort((firstEntry, secondEntry) => {
+    const [firstDateAsString,] = firstEntry;
+    const [secondDateAsString,] = secondEntry;
 
-const minMaxRecords = extent(numberOfRecordsPerDay);
+    return parseTime(firstDateAsString).getTime() - parseTime(secondDateAsString).getTime();
+  });
+
+const numberOfRecordsPerDay = recordsPerDay
+  .map(([day, records]) => ({
+    day,
+    numberOfRecords: records.length
+  }));
+
+console.log(numberOfRecordsPerDay);
+
+const minMaxRecords = extent(numberOfRecordsPerDay.map(({ numberOfRecords }) => numberOfRecords));
 
 const recordsTimeDomain = scaleTime().domain(minMaxDates);
 const recordsNumberDomain = scaleLinear().domain(minMaxRecords);
 
+const NUMBER_OF_TICKS = 6;
+
+// CANVAS COMPONENT
 const Canvas = ({ height, width, doubleOfPadding }) => {
   const yAxisRef = useRef();
   const xAxisRef = useRef();
@@ -42,37 +57,54 @@ const Canvas = ({ height, width, doubleOfPadding }) => {
   const yGridRef = useRef();
   const xGridRef = useRef();
 
+  const recordsNumberScale = recordsNumberDomain.range([height - doubleOfPadding, 0]);
+  const recordsTimeScale = recordsTimeDomain.range([0, width - doubleOfPadding]);
+
+  const areaGen = area()
+    .x(recordDay => recordsTimeScale(parseTime(recordDay.day)))
+    .y0(height - doubleOfPadding)
+    .y1(record => recordsNumberScale(record.numberOfRecords));
+
+  const upperLine = line()
+    .x(recordDay => recordsTimeScale(parseTime(recordDay.day)))
+    .y(record => recordsNumberScale(record.numberOfRecords));
+
+
   useEffect(() => {
     select(yAxisRef.current).call(
-      axisLeft(recordsNumberDomain.range([0, height - doubleOfPadding]))
-        .ticks(6)
+      axisLeft(recordsNumberScale)
+        .ticks(NUMBER_OF_TICKS)
         .tickSize(5));
 
     select(xAxisRef.current)
       .call(
-        axisBottom(recordsTimeDomain.range([0, width - doubleOfPadding]))
-          .ticks(6)
+        axisBottom(recordsTimeScale)
+          .ticks(NUMBER_OF_TICKS)
           .tickSize(5));
 
     select(yGridRef.current).call(
       axisRight(recordsNumberDomain.range([0, height - doubleOfPadding]))
-        .ticks(6)
+        .ticks(NUMBER_OF_TICKS)
         .tickFormat("")
         .tickSize(width - doubleOfPadding));
 
     select(xGridRef.current).call(
       axisBottom(recordsTimeDomain.range([0, width - doubleOfPadding]))
-        .ticks(6)
+        .ticks(NUMBER_OF_TICKS)
         .tickFormat("")
         .tickSize(height - doubleOfPadding));
   });
 
   return <svg width={width} height={height}>
     <g style={{ transform: `translate(${doubleOfPadding / 2}px, ${doubleOfPadding / 2}px)` }}>
+      <g className="grid" ref={yGridRef} />
+      <g className="grid" ref={xGridRef} />
       <g ref={yAxisRef}></g>
       <g style={{ transform: `translateY(${height - doubleOfPadding}px)` }} ref={xAxisRef}></g>
-      <g ref={yGridRef} />
-      <g ref={xGridRef} />
+      <svg width={width - doubleOfPadding} height={height - doubleOfPadding}>
+        <path className="area-chart" d={areaGen(numberOfRecordsPerDay)} />
+        <path className="line-chart" d={upperLine(numberOfRecordsPerDay)} />
+      </svg>
     </g>
   </svg>;
 }
