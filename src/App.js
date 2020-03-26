4780b674ab9c874565aa50baaa94ea29d7e49fbd
Călin Bogdan <calin.bogdan15@gmail.com>
2020-03-26 18:42:17 +0200

    Added component for Institute data per day

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { useRef, useEffect } from 'react';
 import './App.css';
 import { results as healthRecords } from "./assets/records";
+import { results as institutes } from "./assets/institutes";
 import { extent, group } from "d3-array";
 import {
   axisBottom,
   axisLeft,
   axisRight,
-  line,
   scaleTime,
   scaleLinear,
   area,
   utcParse,
   select
 } from "d3";
-
-
 
 const CHART_HEIGHT = 400;
 const CHART_WIDTH = 800;
@@ -24,23 +22,68 @@ const parseTime = utcParse("%d %b %Y");
 const healthRecordsCreationDates = healthRecords
   .map(healthRecord => parseTime(healthRecord.createdon));
 
+// STACKOVERFLOW MADNESSSSSSSSSSSS
+
+var getRandomColor = (function () {
+  var golden_ratio_conjugate = 0.618033988749895;
+  var h = Math.random();
+
+  var hslToRgb = function (h, s, l) {
+    var r, g, b;
+
+    if (s == 0) {
+      r = g = b = l; // achromatic
+    } else {
+      function hue2rgb(p, q, t) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      }
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return '#' + Math.round(r * 255).toString(16) + Math.round(g * 255).toString(16) + Math.round(b * 255).toString(16);
+  };
+
+  return function () {
+    h += golden_ratio_conjugate;
+    h %= 1;
+    return hslToRgb(h, 0.5, 0.60);
+  };
+})();
+
+for (let i = 0; i < 15; i++) {
+  console.log(getRandomColor());
+}
+
+// END OF MADNESSSSSSSSSSSSSS
+
 const minMaxDates = extent(healthRecordsCreationDates);
 
 const recordsPerDay = Array.from(group(healthRecords, healthRecord => healthRecord.createdon))
-  .sort((firstEntry, secondEntry) => {
-    const [firstDateAsString,] = firstEntry;
-    const [secondDateAsString,] = secondEntry;
+  .sort(([firstDate], [secondDate]) => parseTime(firstDate).getTime() - parseTime(secondDate).getTime());
 
-    return parseTime(firstDateAsString).getTime() - parseTime(secondDateAsString).getTime();
-  });
-
-const numberOfRecordsPerDay = recordsPerDay
-  .map(([day, records]) => ({
-    day,
-    numberOfRecords: records.length
+const recordsPerDayPerInstitute = Array.from(group(healthRecords, healthRecord => healthRecord.institute_id, healthRecord => healthRecord.createdon))
+  .map(([instituteId, recsPerDay]) => ({
+    instituteId,
+    numOfRecordsPerDay: Array.from(recsPerDay)
+      .sort(([firstDate], [secondDate]) => parseTime(firstDate).getTime() - parseTime(secondDate).getTime())
+      .map(([day, records]) => ({ day, numberOfRecords: records.length }))
   }));
 
-console.log(numberOfRecordsPerDay);
+
+console.log(recordsPerDayPerInstitute);
+
+const numberOfRecordsPerDay = recordsPerDay
+  .map(([day, records]) => ({ day, numberOfRecords: records.length }));
 
 const minMaxRecords = extent(numberOfRecordsPerDay.map(({ numberOfRecords }) => numberOfRecords));
 
@@ -64,10 +107,6 @@ const Canvas = ({ height, width, doubleOfPadding }) => {
     .x(recordDay => recordsTimeScale(parseTime(recordDay.day)))
     .y0(height - doubleOfPadding)
     .y1(record => recordsNumberScale(record.numberOfRecords));
-
-  const upperLine = line()
-    .x(recordDay => recordsTimeScale(parseTime(recordDay.day)))
-    .y(record => recordsNumberScale(record.numberOfRecords));
 
 
   useEffect(() => {
@@ -102,12 +141,32 @@ const Canvas = ({ height, width, doubleOfPadding }) => {
       <g ref={yAxisRef}></g>
       <g style={{ transform: `translateY(${height - doubleOfPadding}px)` }} ref={xAxisRef}></g>
       <svg width={width - doubleOfPadding} height={height - doubleOfPadding}>
-        <path className="area-chart" d={areaGen(numberOfRecordsPerDay)} />
-        <path className="line-chart" d={upperLine(numberOfRecordsPerDay)} />
+        {recordsPerDayPerInstitute.map((institute, index) =>
+          <InstituteRecordCreationDateChart
+            key={index}
+            areaGen={areaGen}
+            recordsPerDayArray={institute.numOfRecordsPerDay}
+            opacity={1 / institutes.length} />)}
       </svg>
     </g>
   </svg>;
 }
+
+const InstituteRecordCreationDateChart = ({ recordsPerDayArray, areaGen, opacity }) => {
+  const color = getRandomColor();
+  return <g>
+    <path
+      d={areaGen(recordsPerDayArray)}
+      style={{ 
+        fill: color,
+        opacity
+      }} />
+    <path
+      className="line-chart" 
+      d={areaGen.lineY1()(recordsPerDayArray)}
+      style={{ stroke: color }} />
+  </g>
+};
 
 function App() {
   return (
