b4c88b7ecf7d13d57bdae224902bdb46c4abe7e9
Călin Bogdan <calin.bogdan15@gmail.com>
2020-04-01 15:25:26 +0300

    refactored tick styles

--- src/components/InstituteSelector/InstituteListItem.js
+++ src/components/InstituteSelector/InstituteListItem.js
@@ -1,21 +1,28 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useCallback } from "react";
 import InstitutesContext from "./institutesContext";
+import styled from "styled-components";
+
+const TickWrapper = styled.svg`
+  height: 9px;
+  width: 11px;
+  margin-left: 8px;
+  fill: #777;
+`;
 
 const Tick = ({ visible }) => (
-  <svg className={`tick ${visible ? "" : "hidden"}`}>
-    <polygon points="0,5 4,9 11,2 9,0 4,5 2,3" />
-  </svg>
+  <TickWrapper>
+    <polygon points="0,5 4,9 11,2 9,0 4,5 2,3" fillOpacity={ visible || 0 }/>
+  </TickWrapper>
 );
 
 const InstituteListItem = ({ id, name, abbreviation, selected }) => {
   const { setInstituteSelected } = useContext(InstitutesContext);
 
   const clickListener = useCallback(() => {
-    setInstituteSelected(id, !selected)
+    setInstituteSelected(id, !selected);
   }, [id, selected, setInstituteSelected]);
   return (
-    <li className="selector-institute-list-item"
-      onClick={clickListener}>
+    <li className="selector-institute-list-item" onClick={clickListener}>
       <span>{`${name} (${abbreviation})`}</span>
       <Tick visible={selected} />
     </li>
